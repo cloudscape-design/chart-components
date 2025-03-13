902bd106b4dc3fd225959b5718b638c271ed5ae3
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-03-13 14:54:49 +0100

    tests and pages refactoring

--- src/core/__tests__/chart-core-legend-tooltip.test.tsx
+++ src/core/__tests__/chart-core-legend-tooltip.test.tsx
@@ -1,36 +1,13 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { render, waitFor } from "@testing-library/react";
-import Highcharts from "highcharts";
+import { waitFor } from "@testing-library/react";
+import highcharts from "highcharts";
 
-import { ComponentWrapper } from "@cloudscape-design/test-utils-core/dom";
+import { renderChart } from "./common";
 
-import "@cloudscape-design/components/test-utils/dom";
-import { CloudscapeHighcharts, CloudscapeHighchartsProps } from "../../../lib/components/core/chart-core";
-import testClasses from "../../../lib/components/core/test-classes/styles.selectors";
-import tooltipTestClasses from "../../../lib/components/internal/components/popover/test-classes/styles.selectors";
-import createWrapper, { ElementWrapper } from "../../../lib/components/test-utils/dom";
-
-class TestWrapper extends ElementWrapper {
-  findTooltip = () => {
-    const wrapper = this.findByClassName(testClasses.tooltip);
-    return wrapper ? new TooltipTestWrapper(wrapper.getElement()) : null;
-  };
-}
-
-class TooltipTestWrapper extends ComponentWrapper {
-  findHeader = () => this.findByClassName(tooltipTestClasses.header);
-  findBody = () => this.findByClassName(tooltipTestClasses.body);
-  findFooter = () => this.findByClassName(tooltipTestClasses.footer);
-}
-
-function renderChart(props: Partial<CloudscapeHighchartsProps>) {
-  render(<CloudscapeHighcharts highcharts={Highcharts} className="test-chart" options={{}} {...props} />);
-  return new TestWrapper(createWrapper().findByClassName("test-chart")!.getElement());
-}
 function hoverLegendItem(index: number) {
-  const chart = Highcharts.charts.find((c) => c)!;
+  const chart = highcharts.charts.find((c) => c)!;
   const labelElement = (chart.legend.allItems[index] as any).legendItem.label.element;
   labelElement.dispatchEvent(new MouseEvent("mouseover", { bubbles: true, cancelable: true }));
 }
@@ -55,7 +32,8 @@ const pieSeries: Highcharts.SeriesOptionsType[] = [
 describe("CloudscapeHighcharts: legend tooltip", () => {
   test.each(["line", "pie"])("renders legend tooltip for series type = %s", async (seriesType) => {
     const series = seriesType === "line" ? lineSeries : pieSeries;
-    const wrapper = renderChart({
+    const { wrapper } = renderChart({
+      highcharts,
       options: { series },
       legendTooltip: {
         getContent: (id) => ({ header: `Header ${id}`, body: `Body ${id}`, footer: `Footer ${id}` }),
--- src/core/__tests__/chart-core-rendering.test.tsx
+++ src/core/__tests__/chart-core-rendering.test.tsx
@@ -1,34 +1,27 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { render } from "@testing-library/react";
-import Highcharts from "highcharts";
+import highcharts from "highcharts";
 
-import "@cloudscape-design/components/test-utils/dom";
-import { CloudscapeHighcharts, CloudscapeHighchartsProps } from "../../../lib/components/core/chart-core";
-import createWrapper from "../../../lib/components/test-utils/dom";
-
-function renderChart(props: Partial<CloudscapeHighchartsProps>) {
-  render(<CloudscapeHighcharts highcharts={Highcharts} className="test-chart" options={{}} {...props} />);
-  return createWrapper().findByClassName("test-chart");
-}
+import { renderChart } from "./common";
 
 describe("CloudscapeHighcharts: rendering", () => {
   test("renders default fallback with highcharts=null", () => {
-    const wrapper = renderChart({ highcharts: null });
+    const { wrapper } = renderChart({ highcharts: null });
     expect(wrapper).not.toBe(null);
     expect(wrapper!.findSpinner()).not.toBe(null);
   });
 
   test("renders custom fallback with highcharts=null", () => {
-    const wrapper = renderChart({ highcharts: null, fallback: "Custom fallback" });
+    const { wrapper } = renderChart({ highcharts: null, fallback: "Custom fallback" });
     expect(wrapper).not.toBe(null);
     expect(wrapper!.findSpinner()).toBe(null);
     expect(wrapper!.getElement()).toHaveTextContent("Custom fallback");
   });
 
   test("renders chart with highcharts=Highcharts", () => {
-    const wrapper = renderChart({
+    const { wrapper } = renderChart({
+      highcharts,
       options: { title: { text: "Chart title" } },
     });
     expect(wrapper).not.toBe(null);
