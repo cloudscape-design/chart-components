80e358e2fdbc450fd3bcf3c153f20831e6dff226
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-04-02 09:07:32 +0200

    wip: migration pages

--- pages/common/chart-frame.tsx
+++ pages/common/chart-frame.tsx
@@ -7,15 +7,30 @@ import { useMergeRefs } from "../../lib/components/internal/utils/use-merge-refs
 
 import styles from "./styles.module.scss";
 
-export function ChartFrame({
-  children,
-  height,
-  width,
-}: {
+interface ChartFrameBaseProps {
   children: React.ReactNode;
   height?: number | string;
   width?: number | string;
-}) {
+}
+
+export function ChartFrame({ customAnnotation, ...props }: ChartFrameBaseProps & { customAnnotation?: string }) {
+  return customAnnotation ? (
+    <ChartFrameSimple {...props} annotation={customAnnotation} />
+  ) : (
+    <ChartFrameWithAnnotation {...props} />
+  );
+}
+
+function ChartFrameSimple({ children, height, width, annotation }: ChartFrameBaseProps & { annotation: string }) {
+  return (
+    <div className={styles["chart-frame"]} style={{ height, width, overflow: "hidden" }}>
+      {children}
+      <div className={styles["chart-frame-annotation"]}>{annotation}</div>
+    </div>
+  );
+}
+
+function ChartFrameWithAnnotation({ children, height, width }: ChartFrameBaseProps) {
   const [measuredWidth, measureWidthRef] = useContainerQuery((entry) => entry.contentBoxWidth);
   const [measuredHeight, measureHeightRef] = useContainerQuery((entry) => entry.contentBoxHeight);
   const ref = useMergeRefs(measureWidthRef, measureHeightRef);
