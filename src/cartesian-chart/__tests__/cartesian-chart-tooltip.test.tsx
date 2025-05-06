d1b325f9c8599a3edf4c7905f32c36aa7271f4b7
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-05-06 11:47:26 +0200

    fix thresholds computations

--- src/cartesian-chart/__tests__/cartesian-chart-tooltip.test.tsx
+++ src/cartesian-chart/__tests__/cartesian-chart-tooltip.test.tsx
@@ -5,6 +5,7 @@ import { act } from "react";
 import { waitFor } from "@testing-library/react";
 import highcharts from "highcharts";
 
+import "highcharts/highcharts-more";
 import { CartesianChartProps } from "../../../lib/components/cartesian-chart";
 import { highlightChartPoint, leaveChartPoint, renderCartesianChart } from "./common";
 
@@ -42,4 +43,37 @@ describe("CartesianChart: tooltip", () => {
       expect(wrapper.findTooltip()).toBe(null);
     });
   });
+
+  test.each([{ x: 0.01 }, { x: 1 }, { x: 999 }])(
+    "renders all supported series types in tooltip details, x=$x",
+    async ({ x }) => {
+      const { wrapper } = renderCartesianChart({
+        highcharts,
+        series: [
+          { type: "area", name: "Area", data: [{ x, y: 1 }] },
+          { type: "areaspline", name: "\nArea spline", data: [{ x, y: 2 }] },
+          { type: "column", name: "\nColumn", data: [{ x, y: 3 }] },
+          { type: "errorbar", name: "\nError bar", data: [{ x, low: 4, high: 5 }] },
+          { type: "line", name: "\nLine", data: [{ x, y: 6 }] },
+          { type: "scatter", name: "\nScatter", data: [{ x, y: 7 }] },
+          { type: "spline", name: "\nSpline", data: [{ x, y: 8 }] },
+          { type: "x-threshold", name: "\nX threshold", value: x },
+          { type: "y-threshold", name: "\nY threshold", value: 9 },
+        ],
+        xAxis: { valueDecimals: x > 0 ? 0 : 2 },
+        yAxis: { valueDecimals: 0 },
+      });
+
+      act(() => highlightChartPoint(0, 0));
+
+      await waitFor(() => {
+        expect(wrapper.findTooltip()).not.toBe(null);
+        expect(wrapper.findTooltip()!.findHeader()!.getElement().textContent).toBe(x.toFixed(x > 0 ? 0 : 2));
+        expect(wrapper.findTooltip()!.findSeries()).toHaveLength(9);
+        expect(wrapper.findTooltip()!.findBody()!.getElement().textContent).toBe(
+          `Area1\nArea spline2\nColumn3\nError bar4 : 5\nLine6\nScatter7\nSpline8\nX threshold\nY threshold9`,
+        );
+      });
+    },
+  );
 });
