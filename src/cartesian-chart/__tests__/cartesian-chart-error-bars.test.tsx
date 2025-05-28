8990c23cc18e3a8be9c296e409ca7b8fb5912ed7
Joan Perals Tresserra <jotresse@amazon.com>
2025-05-28 09:09:41 +0200

    Refactor error bar unit tests

--- src/cartesian-chart/__tests__/cartesian-chart-error-bars.test.tsx
+++ src/cartesian-chart/__tests__/cartesian-chart-error-bars.test.tsx
@@ -19,98 +19,100 @@ describe("CartesianChart: Error bars", () => {
     vi.resetAllMocks();
   });
 
-  test("renders error bar information in the tooltip", async () => {
-    renderCartesianChart({
-      highcharts,
-      series: [
-        { type: "column", name: "Column 1", data: [2], id: "column-1" },
-        { type: "errorbar", name: "Error range", data: [{ low: 1, high: 3 }], linkedTo: "column-1" },
-      ],
+  describe("Tooltip", () => {
+    test("renders error bar information in the tooltip", async () => {
+      renderCartesianChart({
+        highcharts,
+        series: [
+          { type: "column", name: "Column 1", data: [2], id: "column-1" },
+          { type: "errorbar", name: "Error range", data: [{ low: 1, high: 3 }], linkedTo: "column-1" },
+        ],
+      });
+
+      await highlightFirstPoint();
+      expect(getTooltipSeries(0).findKey().getElement().textContent).toBe("Column 1");
+      expect(getTooltipSeries(0).findValue().getElement().textContent).toBe("2");
+      expect(getTooltipSeries(0).findDetails().getElement().textContent).toBe("Error range1 - 3");
     });
 
-    await highlightFirstPoint();
-    expect(getTooltipSeries(0).findKey().getElement().textContent).toBe("Column 1");
-    expect(getTooltipSeries(0).findValue().getElement().textContent).toBe("2");
-    expect(getTooltipSeries(0).findDetails().getElement().textContent).toBe("Error range1 - 3");
-  });
+    test("attaches error series to the previous series by using `:previous` as value for `linkedTo`", async () => {
+      renderCartesianChart({
+        highcharts,
+        series: [
+          { type: "column", name: "Column 1", data: [2] },
+          { type: "errorbar", name: "Error range", data: [{ low: 1, high: 3 }], linkedTo: ":previous" },
+        ],
+      });
 
-  test("attaches error series to the previous series by using `:previous` as value for `linkedTo`", async () => {
-    renderCartesianChart({
-      highcharts,
-      series: [
-        { type: "column", name: "Column 1", data: [2] },
-        { type: "errorbar", name: "Error range", data: [{ low: 1, high: 3 }], linkedTo: ":previous" },
-      ],
+      await highlightFirstPoint();
+      expect(getTooltipSeries(0).findKey().getElement().textContent).toBe("Column 1");
+      expect(getTooltipSeries(0).findValue().getElement().textContent).toBe("2");
+      expect(getTooltipSeries(0).findDetails().getElement().textContent).toBe("Error range1 - 3");
     });
 
-    await highlightFirstPoint();
-    expect(getTooltipSeries(0).findKey().getElement().textContent).toBe("Column 1");
-    expect(getTooltipSeries(0).findValue().getElement().textContent).toBe("2");
-    expect(getTooltipSeries(0).findDetails().getElement().textContent).toBe("Error range1 - 3");
-  });
+    test("renders only the error range if error bar series name is not provided", async () => {
+      renderCartesianChart({
+        highcharts,
+        series: [
+          { type: "column", name: "Column 1", data: [2], id: "column-1" },
+          { type: "errorbar", data: [{ low: 1, high: 3 }], linkedTo: "column-1" },
+        ],
+      });
 
-  test("renders only the error range if error bar series name is not provided", async () => {
-    renderCartesianChart({
-      highcharts,
-      series: [
-        { type: "column", name: "Column 1", data: [2], id: "column-1" },
-        { type: "errorbar", data: [{ low: 1, high: 3 }], linkedTo: "column-1" },
-      ],
+      await highlightFirstPoint();
+      expect(getTooltipSeries(0).findDetails().getElement().textContent).toBe("1 - 3");
     });
 
-    await highlightFirstPoint();
-    expect(getTooltipSeries(0).findDetails().getElement().textContent).toBe("1 - 3");
-  });
+    test("renders multiple error bars per series", async () => {
+      renderCartesianChart({
+        highcharts,
+        series: [
+          { type: "column", name: "Column 1", data: [2], id: "column-1" },
+          { type: "errorbar", name: "Error range 1", data: [{ low: 1, high: 3 }], linkedTo: "column-1" },
+          { type: "errorbar", name: "Error range 2", data: [{ low: 0, high: 4 }], linkedTo: "column-1" },
+        ],
+      });
 
-  test("renders multiple error bars per series", async () => {
-    renderCartesianChart({
-      highcharts,
-      series: [
-        { type: "column", name: "Column 1", data: [2], id: "column-1" },
-        { type: "errorbar", name: "Error range 1", data: [{ low: 1, high: 3 }], linkedTo: "column-1" },
-        { type: "errorbar", name: "Error range 2", data: [{ low: 0, high: 4 }], linkedTo: "column-1" },
-      ],
+      await highlightFirstPoint();
+      expect(getTooltipSeries(0).findKey().getElement().textContent).toBe("Column 1");
+      expect(getTooltipSeries(0).findValue().getElement().textContent).toBe("2");
+      expect(getTooltipSeries(0).findDetails().getElement().textContent).toBe(
+        "Error range 1" + "1 - 3" + "Error range 2" + "0 - 4",
+      );
     });
 
-    await highlightFirstPoint();
-    expect(getTooltipSeries(0).findKey().getElement().textContent).toBe("Column 1");
-    expect(getTooltipSeries(0).findValue().getElement().textContent).toBe("2");
-    expect(getTooltipSeries(0).findDetails().getElement().textContent).toBe(
-      "Error range 1" + "1 - 3" + "Error range 2" + "0 - 4",
-    );
-  });
+    test("supports customization of the tooltip", async () => {
+      renderCartesianChart({
+        highcharts,
+        series: [
+          { type: "column", name: "Column 1", data: [2], id: "column-1" },
+          { type: "errorbar", name: "Column 2", data: [{ low: 1, high: 3 }], linkedTo: "column-1" },
+        ],
+        tooltip: {
+          series: ({ item }) => ({
+            key: `Custom key ${item.series.name}`,
+            value: `Custom value ${item.y}`,
+            details: `Custom details ${item.errorRanges![0].low} - ${item.errorRanges![0].high}`,
+          }),
+        },
+      });
 
-  test("supports customization of the tooltip", async () => {
-    renderCartesianChart({
-      highcharts,
-      series: [
-        { type: "column", name: "Column 1", data: [2], id: "column-1" },
-        { type: "errorbar", name: "Column 2", data: [{ low: 1, high: 3 }], linkedTo: "column-1" },
-      ],
-      tooltip: {
-        series: ({ item }) => ({
-          key: `Custom key ${item.series.name}`,
-          value: `Custom value ${item.y}`,
-          details: `Custom details ${item.errorRanges![0].low} - ${item.errorRanges![0].high}`,
-        }),
-      },
+      await highlightFirstPoint();
+      expect(getTooltipSeries(0).findKey().getElement().textContent).toBe("Custom key Column 1");
+      expect(getTooltipSeries(0).findValue().getElement().textContent).toBe("Custom value 2");
+      expect(getTooltipSeries(0).findDetails().getElement().textContent).toBe("Custom details 1 - 3");
     });
 
-    await highlightFirstPoint();
-    expect(getTooltipSeries(0).findKey().getElement().textContent).toBe("Custom key Column 1");
-    expect(getTooltipSeries(0).findValue().getElement().textContent).toBe("Custom value 2");
-    expect(getTooltipSeries(0).findDetails().getElement().textContent).toBe("Custom details 1 - 3");
-  });
+    test("issues a warning if linkedTo does not refer to an existing series id", () => {
+      const spy = vi.spyOn(console, "warn").mockImplementation(() => {});
+      renderCartesianChart({
+        highcharts,
+        series: [{ type: "errorbar", linkedTo: "nonExistingId", data: [{ low: 1, high: 2 }] }],
+      });
 
-  test("issues a warning if linkedTo does not refer to an existing series id", () => {
-    const spy = vi.spyOn(console, "warn").mockImplementation(() => {});
-    renderCartesianChart({
-      highcharts,
-      series: [{ type: "errorbar", linkedTo: "nonExistingId", data: [{ low: 1, high: 2 }] }],
+      act(() => hc.highlightChartPoint(0, 0));
+      expect(spy).toHaveBeenCalledOnce();
     });
-
-    act(() => hc.highlightChartPoint(0, 0));
-    expect(spy).toHaveBeenCalledOnce();
   });
 });
 
