fc5d694e9f7dc3bba75436a8d2db7f837bc23a4c
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-06-04 09:53:14 +0200

    fix eslint errors

--- src/__tests__/utils.ts
+++ src/__tests__/utils.ts
@@ -2,11 +2,9 @@
 /* eslint-disable header/header */
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { capitalize } from "lodash";
+
 import * as fs from "node:fs";
 import * as path from "node:path";
-
-import * as components from "../../lib/components";
 
 const componentsDir = path.resolve(__dirname, "../../lib/components");
 
