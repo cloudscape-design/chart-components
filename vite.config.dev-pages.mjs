0996c7b58b3ea94e8c92fce74e1003d5915f00ed
Joan Perals Tresserra <jotresse@amazon.com>
2025-12-11 09:43:16 +0100

    Build dev pages

---
+++ vite.config.dev-pages.mjs
@@ -0,0 +1,33 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const componentsPath = resolve(__dirname, "lib/components");
+
+const globals = { [componentsPath]: "components", highcharts: "highcharts" };
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react({ fastRefresh: false })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "pages/main.tsx"),
+      name: "ChartComponentsDevPages",
+      fileName: "main",
+    },
+    outDir: "lib/dev-pages/bundle",
+    rollupOptions: {
+      external: [componentsPath, "highcharts"],
+      output: {
+        globals,
+      },
+    },
+  },
+});
